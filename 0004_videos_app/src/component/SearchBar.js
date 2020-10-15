b683fe5d13da25c1b9dddce05a535b05c47e74e9
muhammed-topgul <muhammedtopgul.29@gmail.com>
2020-10-15 15:44:47 +0300

    search bar alani olusturuldu

--- 0004_videos_app/src/component/SearchBar.js
+++ 0004_videos_app/src/component/SearchBar.js
@@ -1,10 +1,33 @@
 import React, {Component} from 'react';
 
 class SearchBar extends Component {
+
+    state = {term: ''}
+
+    onInputChange = (event) => {
+        this.setState({
+            term: event.target.value
+        });
+    }
+
+    onFormSubmit = (event) => {
+        event.preventDefault();
+
+        // TODO: callback from parent component
+    }
+
     render() {
         return (
-            <div>
-               Search Bar
+            <div className={"search-bar ui segment"}>
+                <form onSubmit={this.onFormSubmit} className={"ui form"}>
+                    <div className={"field"}>
+                        <label>Video Search</label>
+                        <input type={"text"}
+                               value={this.state.term}
+                               onChange={this.onInputChange}
+                        />
+                    </div>
+                </form>
             </div>
         );
     }
