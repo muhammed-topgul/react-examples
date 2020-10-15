8bf70896d24f20b9076042e5cd2d420ff3ee2a12
muhammed-topgul <muhammedtopgul.29@gmail.com>
2020-10-15 14:01:42 +0300

    api' den alınan resimler listelendi

--- 0003_pics_app/src/component/App.js
+++ 0003_pics_app/src/component/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import SearchBar from "./SearchBar";
 import {API_URL} from "../common/env";
 import unsplash from "../service/unsplash";
+import ImageList from "./ImageList";
 
 class App extends React.Component {
 
@@ -24,6 +25,7 @@ class App extends React.Component {
             <div className={"ui container"} style={{marginTop: '10px'}}>
                 <SearchBar callbackOnSearchSubmit={this.callbackOnSearchSubmit}/>
                 Found: {this.state.images.length} images
+                <ImageList images={this.state.images}/>
             </div>
         );
     }
