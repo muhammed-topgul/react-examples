006464cdd8e6c892cd35486984123affb060e37f
muhammed-topgul <muhammedtopgul.29@gmail.com>
2020-11-09 20:39:36 +0300

    useEffect fonksiyon kabiliyetleri incelendi

--- 0005_widgets_app/src/component/Search.js
+++ 0005_widgets_app/src/component/Search.js
@@ -1,8 +1,26 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 
 const Search = () => {
 
     const [term, setTerm] = useState('');
+
+    alert("1");
+    console.log("I RUN WITH EVERY RENDER...");
+
+    useEffect(() => {
+        console.log("I ONLY RUN ONCE...");
+        alert("2");
+    }, []);
+
+    useEffect(() => {
+        console.log("I RUN AFTER EVERY RENDER AND AT THE INITIAL RENDER");
+        alert("3");
+    });
+
+    useEffect(() => {
+        console.log("I RUN AFTER EVERY RENDER IF 'term' CHANGES")
+        alert("4");
+    }, [term]);
 
     return (
         <div>
