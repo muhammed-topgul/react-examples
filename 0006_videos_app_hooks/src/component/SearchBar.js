4f15e6fcc8a73980d2066e5bfad9fb4a20050a01
muhammed-topgul <muhammedtopgul.29@gmail.com>
2020-11-11 21:15:40 +0300

    0006-video-app-hook --> search bar hook ile update edildi

--- 0006_videos_app_hooks/src/component/SearchBar.js
+++ 0006_videos_app_hooks/src/component/SearchBar.js
@@ -1,36 +1,30 @@
-import React, {Component} from 'react';
+import React, {useState} from 'react';
 
-class SearchBar extends Component {
+const SearchBar = ({callbackSearchSubmit}) => {
+    const [term, setTerm] = useState();
 
-    state = {term: ''}
-
-    onInputChange = (event) => {
-        this.setState({
-            term: event.target.value
-        });
+    const onInputChange = (event) => {
+        setTerm(event.target.value);
     }
 
-    onFormSubmit = (event) => {
+    const onFormSubmit = (event) => {
         event.preventDefault();
-
-       this.props.callbackSearchSubmit(this.state.term);
+        callbackSearchSubmit(term);
     }
 
-    render() {
-        return (
-            <div className={"search-bar ui segment"}>
-                <form onSubmit={this.onFormSubmit} className={"ui form"}>
-                    <div className={"field"}>
-                        <label>Video Search</label>
-                        <input type={"text"}
-                               value={this.state.term}
-                               onChange={this.onInputChange}
-                        />
-                    </div>
-                </form>
-            </div>
-        );
-    }
+    return (
+        <div className={"search-bar ui segment"}>
+            <form onSubmit={onFormSubmit} className={"ui form"}>
+                <div className={"field"}>
+                    <label>Video Search</label>
+                    <input type={"text"}
+                           value={term}
+                           onChange={onInputChange}
+                    />
+                </div>
+            </form>
+        </div>
+    );
 }
 
 export default SearchBar;
