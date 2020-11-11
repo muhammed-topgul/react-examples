fe5863b7bf703d82f34c6ff510a40eef2390744a
muhammed-topgul <muhammedtopgul.29@gmail.com>
2020-11-11 13:45:12 +0300

    dil input alanı eklendi

--- 0005_widgets_app/src/common/env.js
+++ 0005_widgets_app/src/common/env.js
@@ -1,2 +1,3 @@
 export const WIKIPEDIA_API_URL = "https://en.wikipedia.org/w/api.php";
 export const WIKIPEDIA_CURRENT_ELEMENT = "https://en.wikipedia.org?curid=";
+export const GOOGLE_TRANSLATE_API_KEY = "AIzaSyCHUCmpR7cT_yDFHC98CZJy2LTms-IwDlM";
--- 0005_widgets_app/src/component/Translate.js
+++ 0005_widgets_app/src/component/Translate.js
@@ -5,9 +5,18 @@ import {options} from "../data/langs";
 const Translate = () => {
 
     const [language, setLanguage] = useState(options[0]);
+    const [text, setText] = useState('');
 
     return (
         <div>
+            <div className={"ui form"}>
+                <div className={"filed"}>
+                    <label>Enter Text</label>
+                    <input value={text}
+                           onChange={(event) => setText(event.target.value)}/>
+                </div>
+            </div>
+
             <Dropdown
                 label={"Select a language"}
                 selected={language}
