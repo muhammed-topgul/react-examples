8433865dc579d37e3dc6222c7deb3f1f19439210
muhammed-topgul <muhammedtopgul.29@gmail.com>
2020-11-15 20:27:43 +0300

    listeden sarki secilince action creator cagirmak ve propsu guncellemek

--- 0008_react_redux_integrated_app/src/component/SongList.js
+++ 0008_react_redux_integrated_app/src/component/SongList.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 
 // REDUX SETTING
 import {connect} from 'react-redux';
+import {selectSong} from '../action';
 
 class SongList extends Component {
 
@@ -10,7 +11,9 @@ class SongList extends Component {
             return (
                 <div className={'item'} key={song.title}>
                     <div className={'right floated content'}>
-                        <button className={'ui button primary'}>
+                        <button
+                            className={'ui button primary'}
+                            onClick={() => this.props.selectSong(song)}>
                             Select
                         </button>
                     </div>
@@ -30,7 +33,10 @@ class SongList extends Component {
 }
 
 const mapStateToProps = (state) => {
-    return {songs: state.songs}
+    return {
+        songs: state.songs,
+        selectedSong: state.selectedSong
+    }
 }
 
-export default connect(mapStateToProps)(SongList);
+export default connect(mapStateToProps, {selectSong})(SongList);
