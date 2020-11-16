7353d5febc4cb126323c8f1cdf6045fc8f2bb6e9
muhammed-topgul <muhammedtopgul.29@gmail.com>
2020-11-16 11:47:52 +0300

    post list component olsturuldu

---
+++ 0009_blog_app/src/component/PostList.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class PostList extends Component {
+    render() {
+        return (
+            <div>
+                Post List Component
+            </div>
+        );
+    }
+}
+
+export default PostList;
