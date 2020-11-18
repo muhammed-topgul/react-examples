6a51343ce3be5d6ee7d00ef66155bcb67bcfbf93
muhammed-topgul <muhammedtopgul.29@gmail.com>
2020-11-18 23:46:42 +0300

    1. basit bir ReactRouter ornegi

--- 0010_streams/client/src/component/App.js
+++ 0010_streams/client/src/component/App.js
@@ -1,9 +1,24 @@
 import React from 'react';
 
+// REACT ROUTER DOM SETTINGS
+import {BrowserRouter, Route} from "react-router-dom";
+
+const PageOne = () => {
+    return <div>Page One</div>
+}
+
+
+const PageTwo = () => {
+    return <div>Page Two</div>
+}
+
 const App = () => {
     return (
         <div>
-            App
+            <BrowserRouter>
+                <Route path={'/'} exact component={PageOne}/>
+                <Route path={'/page-two'} component={PageTwo}/>
+            </BrowserRouter>
         </div>
     );
 };
