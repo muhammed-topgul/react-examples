90fc505ef13b560745b3c80d5dfc77a2b9d4d87b
muhammed-topgul <muhammedtopgul.29@gmail.com>
2020-11-18 19:46:12 +0300

    memoize ile postun kullanicilarini bulurken her kullanici icin tek istek atmayi sagladim. yani sadece unique istekler atacak ve bunu id vasitasiyla saglayacak

--- 0009_blog_app/src/action/index.js
+++ 0009_blog_app/src/action/index.js
@@ -1,12 +1,21 @@
 import {FETCH_POSTS, FETCH_USER} from "../common/env";
 import jsonPlaceholder from "../service/jsonPlaceholder";
+import _ from "lodash";
 
 export const fetchPosts = () => async (dispatch) => {
     const response = await jsonPlaceholder.get('/posts');
     dispatch({type: FETCH_POSTS, payload: response.data});
 }
 
-export const fetchUser = (id) => async (dispatch) => {
+export const fetchUser = (id) => (dispatch) => {
+    _fetchUser(id, dispatch);
+}
+// fetchUser fonksiyonunu lodash ile baska bir mantikla yazdim
+// cunku ilk versiyonda her post icin bu method istek atiyor yani bir kullanicinin birden fazla postu olabilir ve hepsini bulmak icin tek tek yeni istek atiyor
+// bu yuzden ben memoize ile atilan istekleri cache'liyorum ve ayni id'li kullanicilar icin yeni istek attirmiyorum
+//  her id icin bir istek
+// iste memoize'in amaci bu :)
+const _fetchUser = _.memoize(async (id, dispatch) => {
     const response = await jsonPlaceholder.get(`/users/${id}`);
     dispatch({type: FETCH_USER, payload: response.data});
-}
+});
