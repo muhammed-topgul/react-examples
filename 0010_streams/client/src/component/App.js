ebdab2d8e3c54e8e56076e56dcfccb34e426c1b0
muhammed-topgul <muhammedtopgul.29@gmail.com>
2020-11-19 20:54:39 +0300

    3. componentler router ile baglandi

--- 0010_streams/client/src/component/App.js
+++ 0010_streams/client/src/component/App.js
@@ -3,33 +3,22 @@ import React from 'react';
 // REACT ROUTER DOM SETTINGS
 import {BrowserRouter, Route, Link} from "react-router-dom";
 
-const PageOne = () => {
-    return (
-        <div>
-            Page One
-            <br/>
-            <Link to={'/page-two'}>Navigate to Page Two</Link>
-        </div>
-    );
-}
-
-
-const PageTwo = () => {
-    return (
-        <div>
-            Page Two
-            <br/>
-            <Link to={'/'}>Navigate to Page One</Link>
-        </div>
-    );
-}
+// STREAM COMPONENTS
+import StreamList from "./stream/StreamList";
+import StreamCreate from "./stream/StreamCreate";
+import StreamEdit from "./stream/StreamEdit";
+import StreamDelete from "./stream/StreamDelete";
+import StreamShow from "./stream/StreamShow";
 
 const App = () => {
     return (
         <div>
             <BrowserRouter>
-                <Route path={'/'} exact component={PageOne}/>
-                <Route path={'/page-two'} component={PageTwo}/>
+                <Route path={'/'} exact component={StreamList}/>
+                <Route path={'/streams/new'} exact component={StreamCreate}/>
+                <Route path={'/streams/edit'} exact component={StreamEdit}/>
+                <Route path={'/streams/delete'} exact component={StreamDelete}/>
+                <Route path={'/streams/show'} exact component={StreamShow}/>
             </BrowserRouter>
         </div>
     );
