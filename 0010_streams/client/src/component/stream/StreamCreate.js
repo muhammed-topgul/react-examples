2fda2c08121d2bc7b6cb2a019b0feec4e78e5d03
muhammed-topgul <muhammedtopgul.29@gmail.com>
2020-11-20 20:19:57 +0300

    15. stream create component' ine redux form uygulandi. baya eglenceliymis :)

--- 0010_streams/client/src/component/stream/StreamCreate.js
+++ 0010_streams/client/src/component/stream/StreamCreate.js
@@ -1,11 +1,30 @@
-import React from 'react';
+import React, {Component} from 'react';
 
-const StreamCreate = () => {
-    return (
-        <div>
-            StreamCreate
-        </div>
-    );
-};
+// REDUX FORM
+import {Field, reduxForm} from "redux-form";
 
-export default StreamCreate;
+class StreamCreate extends Component {
+
+    // renderInput(formProps) {
+    //     return (
+    //         <input
+    //             onChange={formProps.input.onChange}
+    //             value={formProps.input.value}/>
+    //     )
+    // }
+
+    renderInput({input}) {
+        return <input {...input}/>
+    }
+
+    render() {
+        return (
+            <form>
+                <Field name={'title'} component={this.renderInput}/>
+                <Field name={'description'} component={this.renderInput}/>
+            </form>
+        );
+    }
+}
+
+export default reduxForm({form: 'streamCreateForm'})(StreamCreate);
