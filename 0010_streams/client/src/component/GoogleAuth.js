3fd435a31a1b977eb8c80f3bd170e600deae320b
muhammed-topgul <muhammedtopgul.29@gmail.com>
2020-11-20 14:21:14 +0300

    7. sign in ve sign out butonlari ile google uzerinden auth olma veya cikma

--- 0010_streams/client/src/component/GoogleAuth.js
+++ 0010_streams/client/src/component/GoogleAuth.js
@@ -32,13 +32,31 @@ class GoogleAuth extends Component {
         });
     }
 
+    onSignIn = () => {
+        this.auth.signIn();
+    }
+
+    onSignOut = () => {
+        this.auth.signOut();
+    }
+
     renderAuthButton() {
         if (this.state.isSignedIn === null) {
-            return <div>I dont know if we are signed in!</div>
+            return null;
         } else if (this.state.isSignedIn) {
-            return <div>I am signed in!</div>
+            return (
+                <button className={'ui red google button'} onClick={this.onSignOut}>
+                    <i className={'google icon'}/>
+                    Sign Out
+                </button>
+            );
         } else {
-            return <div>I am not signed in!</div>
+            return (
+                <button className={'ui primary google button'} onClick={this.onSignIn}>
+                    <i className={'google icon'}/>
+                    Sign In with Google
+                </button>
+            );
         }
     }
 
