8a34e49a964963f8ec2bb928d832680031f560f2
muhammed-topgul <muhammedtopgul.29@gmail.com>
2020-11-20 15:23:13 +0300

    8. authentication icin redux setting' ler yapildi

--- 0010_streams/client/src/component/GoogleAuth.js
+++ 0010_streams/client/src/component/GoogleAuth.js
@@ -32,11 +32,11 @@ class GoogleAuth extends Component {
         });
     }
 
-    onSignIn = () => {
+    onSignInClick = () => {
         this.auth.signIn();
     }
 
-    onSignOut = () => {
+    onSignOutClick = () => {
         this.auth.signOut();
     }
 
@@ -45,14 +45,14 @@ class GoogleAuth extends Component {
             return null;
         } else if (this.state.isSignedIn) {
             return (
-                <button className={'ui red google button'} onClick={this.onSignOut}>
+                <button className={'ui red google button'} onClick={this.onSignOutClick}>
                     <i className={'google icon'}/>
                     Sign Out
                 </button>
             );
         } else {
             return (
-                <button className={'ui primary google button'} onClick={this.onSignIn}>
+                <button className={'ui primary google button'} onClick={this.onSignInClick}>
                     <i className={'google icon'}/>
                     Sign In with Google
                 </button>
