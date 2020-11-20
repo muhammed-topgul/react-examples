1257f3ffa1e98096bd548ba9c48d8c34a5cb6e53
muhammed-topgul <muhammedtopgul.29@gmail.com>
2020-11-20 19:52:57 +0300

    14. redux-form ayarlamalari yapildi

--- 0010_streams/client/src/reducer/index.js
+++ 0010_streams/client/src/reducer/index.js
@@ -1,6 +1,10 @@
 import {combineReducers} from "redux";
 import authReducer from "./authReducer";
 
+// REDUX FORM
+import {reducer as formReducer} from "redux-form";
+
 export default combineReducers({
+    form: formReducer,
     auth: authReducer
 });
