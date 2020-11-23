fe3bd72087ea7856a74f7ca809f1afbca13caf0e
muhammed-topgul <muhammedtopgul.29@gmail.com>
2020-11-24 00:00:18 +0300

    22. stream reducers ile ilgili diger gereklilikler yapildi.

--- 0010_streams/client/src/reducer/index.js
+++ 0010_streams/client/src/reducer/index.js
@@ -1,10 +1,12 @@
 import {combineReducers} from "redux";
 import authReducer from "./authReducer";
+import streamReducer from "./streamReducer";
 
 // REDUX FORM
 import {reducer as formReducer} from "redux-form";
 
 export default combineReducers({
     form: formReducer,
-    auth: authReducer
+    auth: authReducer,
+    streams: streamReducer
 });
--- 0010_streams/client/src/reducer/streamReducer.js
+++ 0010_streams/client/src/reducer/streamReducer.js
@@ -1,8 +1,13 @@
-import {CREATE_STREAM, DELETE_STREAM, EDIT_STREAM, FETCH_STREAM} from "../common/env";
+import {CREATE_STREAM, DELETE_STREAM, EDIT_STREAM, FETCH_STREAM, FETCH_STREAMS} from "../common/env";
 import _ from 'lodash';
 
 export default (state = {}, action) => {
     switch (action.type) {
+        case FETCH_STREAMS:
+            // normalde action.payload bir array doner
+            // lodash ile ben donen arrayde ki verileri id ile mapleyerek yeni bir obje olusturuyorum
+            return {...state, ..._.mapKeys(action.payload, 'id')}
+
         case FETCH_STREAM:
             return {...state, [action.payload.id]: action.payload}
 
