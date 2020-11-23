57386d0e129a2d3448c97b54b4ca7630d9a757e5
muhammed-topgul <muhammedtopgul.29@gmail.com>
2020-11-24 00:14:11 +0300

    23. stream' ler ekranda listelendi

--- 0010_streams/client/src/component/stream/StreamList.js
+++ 0010_streams/client/src/component/stream/StreamList.js
@@ -1,11 +1,42 @@
 import React from 'react';
 
-const StreamList = () => {
-    return (
-        <div>
-            StreamList
-        </div>
-    );
-};
+// REDUX SETTING
+import {connect} from 'react-redux';
+import {fetchStreams} from "../../action";
 
-export default StreamList;
+class StreamList extends React.Component {
+
+    componentDidMount() {
+        this.props.fetchStreams();
+    }
+
+    renderList = () => {
+        return this.props.streams.map(stream => {
+            return (
+                <div className={'item'} key={stream.id}>
+                    <i className={'large middle aligned icon camera'}/>
+                    <div className={'content'}>
+                        {stream.title}
+                        <div className={'description'}>{stream.description}</div>
+                    </div>
+                </div>
+            )
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                <h2>Streams</h2>
+                <div className={'ui celled list'}>{this.renderList()}</div>
+            </div>
+        );
+    }
+}
+
+const mapStateToProps = (state) => ({
+    // Object.values obje alir array verir
+    streams: Object.values(state.streams)
+});
+
+export default connect(mapStateToProps, {fetchStreams})(StreamList);
