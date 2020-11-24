061601c8eabc334e17fe65f6f8d1e6e040c4c9a3
muhammed-topgul <muhammedtopgul.29@gmail.com>
2020-11-24 14:50:44 +0300

    25. eger user streami yaratan kisi ise butonları ekranda goster

--- 0010_streams/client/src/component/stream/StreamList.js
+++ 0010_streams/client/src/component/stream/StreamList.js
@@ -10,10 +10,22 @@ class StreamList extends React.Component {
         this.props.fetchStreams();
     }
 
+    renderIsAdmin = (stream) => {
+        if (stream.userId === this.props.currentUserId) {
+            return (
+                <div className={'right floated content'}>
+                    <button className={'ui button primary'}>Edit</button>
+                    <button className={'ui button negative'}>Delete</button>
+                </div>
+            )
+        }
+    }
+
     renderList = () => {
         return this.props.streams.map(stream => {
             return (
                 <div className={'item'} key={stream.id}>
+                    {this.renderIsAdmin(stream)}
                     <i className={'large middle aligned icon camera'}/>
                     <div className={'content'}>
                         {stream.title}
@@ -36,7 +48,8 @@ class StreamList extends React.Component {
 
 const mapStateToProps = (state) => ({
     // Object.values obje alir array verir
-    streams: Object.values(state.streams)
+    streams: Object.values(state.streams),
+    currentUserId: state.auth.userId
 });
 
 export default connect(mapStateToProps, {fetchStreams})(StreamList);
