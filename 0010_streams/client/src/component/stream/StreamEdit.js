c01a75eb888961ca7eefedf3f3d6a83e8acae415
muhammed-topgul <muhammedtopgul.29@gmail.com>
2020-11-24 16:05:57 +0300

    29. state' ten secilen stream' in id' sine gore edit ekrani acildi

--- 0010_streams/client/src/component/stream/StreamEdit.js
+++ 0010_streams/client/src/component/stream/StreamEdit.js
@@ -1,6 +1,10 @@
 import React from 'react';
 
-const StreamEdit = () => {
+// REDUX SETTING
+import {connect} from 'react-redux';
+
+const StreamEdit = (props) => {
+    console.log(props)
     return (
         <div>
             StreamEdit
@@ -8,4 +12,10 @@ const StreamEdit = () => {
     );
 };
 
-export default StreamEdit;
+const mapStateToProps = (state, ownProps) => {
+    return {
+        stream: state.streams[ownProps.match.params.id]
+    }
+}
+
+export default connect(mapStateToProps)(StreamEdit);
