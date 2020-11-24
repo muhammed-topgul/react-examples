a5c271d76463c7fd4ba4c2874e466bc3f4647510
muhammed-topgul <muhammedtopgul.29@gmail.com>
2020-11-24 15:15:39 +0300

    26. eger kullanici auth ise create button ekranda gosterildi

--- 0010_streams/client/src/component/stream/StreamList.js
+++ 0010_streams/client/src/component/stream/StreamList.js
@@ -3,11 +3,24 @@ import React from 'react';
 // REDUX SETTING
 import {connect} from 'react-redux';
 import {fetchStreams} from "../../action";
+import {Link} from "react-router-dom";
 
 class StreamList extends React.Component {
 
     componentDidMount() {
         this.props.fetchStreams();
+    }
+
+    renderCreateButton = () => {
+        if (this.props.isSignedIn) {
+            return (
+                <div style={{textAlign: 'right'}}>
+                    <Link to={'/streams/new'} className={'ui button primary'}>
+                        Create Stream
+                    </Link>
+                </div>
+            )
+        }
     }
 
     renderIsAdmin = (stream) => {
@@ -41,6 +54,7 @@ class StreamList extends React.Component {
             <div>
                 <h2>Streams</h2>
                 <div className={'ui celled list'}>{this.renderList()}</div>
+                {this.renderCreateButton()}
             </div>
         );
     }
@@ -49,7 +63,8 @@ class StreamList extends React.Component {
 const mapStateToProps = (state) => ({
     // Object.values obje alir array verir
     streams: Object.values(state.streams),
-    currentUserId: state.auth.userId
+    currentUserId: state.auth.userId,
+    isSignedIn: state.auth.isSignedIn
 });
 
 export default connect(mapStateToProps, {fetchStreams})(StreamList);
